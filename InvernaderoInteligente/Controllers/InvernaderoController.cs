be792ede67d746791458129f85e514739cbd93e3
Fran-Bar03 <papihachi_vlk@outlook.com>
2025-04-09 21:54:47 -0700

    arreglos api

--- InvernaderoInteligente/Controllers/InvernaderoController.cs
+++ InvernaderoInteligente/Controllers/InvernaderoController.cs
@@ -9,22 +9,25 @@ using MongoDB.Driver;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace InvernaderoInteligente.Controllers {
-  [Route ("api/[controller]")]
-  [ApiController]
-  public class InvernaderoController : ControllerBase {
+namespace InvernaderoInteligente.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvernaderoController : ControllerBase
+    {
 
 
-    private readonly IInvernaderoService _invernaderoService;
-    private readonly InvernaderoService _iinvernaderoService;
-    private readonly IMongoCollection<UsuarioModel> _usuariosCollection;
+        private readonly IInvernaderoService _invernaderoService;
+        private readonly InvernaderoService _iinvernaderoService;
+        private readonly IMongoCollection<UsuarioModel> _usuariosCollection;
 
-    public InvernaderoController (IInvernaderoService invernaderoService, InvernaderoService invernaderoService1, IMongoClient mongoClient, IOptions<ConfiguracionMongo> config) {
-      _invernaderoService = invernaderoService;
-      _iinvernaderoService = invernaderoService1;
-      var db = mongoClient.GetDatabase (config.Value.DataBase);
-      _usuariosCollection = db.GetCollection<UsuarioModel> ("Usuario");
-    }
+        public InvernaderoController(IInvernaderoService invernaderoService, InvernaderoService invernaderoService1, IMongoClient mongoClient, IOptions<ConfiguracionMongo> config)
+        {
+            _invernaderoService = invernaderoService;
+            _iinvernaderoService = invernaderoService1;
+            var db = mongoClient.GetDatabase(config.Value.DataBase);
+            _usuariosCollection = db.GetCollection<UsuarioModel>("Usuario");
+        }
 
 
 
@@ -43,9 +46,8 @@ namespace InvernaderoInteligente.Controllers {
                 .ToListAsync();
 
             // Construir la respuesta, asegurándonos de que solo se devuelvan los nombres completos de los usuarios
-            var resultado = invernaderos.Select(inv => new {
-                Invernadero = new
-                {
+            var resultado = invernaderos.Select(inv => new
+            {
                     inv.InvernaderoId,
                     inv.Nombre,
                     inv.NombrePlanta,
@@ -55,13 +57,12 @@ namespace InvernaderoInteligente.Controllers {
                     inv.MaxTemperatura,
                     inv.MinHumedad,
                     inv.MaxHumedad,
-                    Sensores = inv.Sensores // Solo mantén los sensores aquí
-                },
-                Usuarios = usuariosAsignados
-                    .Where(u => u.Invernaderos.Contains(inv.Nombre)) // Filtramos por invernadero
-                    .Select(u => u.NombreCompleto)  // Solo seleccionamos el nombre completo
-                    .Distinct() // Elimina duplicados
-                    .ToList()
+                    Sensores = inv.Sensores,
+                    Usuarios = usuariosAsignados
+               .Where(u => u.Invernaderos.Contains(inv.Nombre)) // Filtramos por invernadero
+               .Select(u => u.NombreCompleto)  // Solo seleccionamos el nombre completo
+               .Distinct() // Elimina duplicados
+               .ToList()
             }).ToList();
 
             return Ok(resultado);
@@ -72,66 +73,79 @@ namespace InvernaderoInteligente.Controllers {
 
 
         // GET api/<InvernaderoController>/5
-        [HttpGet ("BuscarInvernadero")]
-    public async Task<IActionResult> BuscarInvernadero (string Nombre) {
-      var Invernadero = await _invernaderoService.BuscarInvernadero (Nombre);
-      return Ok (Invernadero);
-    }
+        [HttpGet("BuscarInvernadero")]
+        public async Task<IActionResult> BuscarInvernadero(string Nombre)
+        {
+            var Invernadero = await _invernaderoService.BuscarInvernadero(Nombre);
+            return Ok(Invernadero);
+        }
 
 
-    // PUT api/<InvernaderoController>/5
-    [HttpPut ("ActualizarInvernadero")]
-    public async Task<IActionResult> ActualizarInvernadero (InvernaderoModel invernaderomodel) {
-      try {
-        var resultado = await _iinvernaderoService.ActualizarInvernadero (invernaderomodel, invernaderomodel.Usuarios, invernaderomodel.Sensores);
+        // PUT api/<InvernaderoController>/5
+        [HttpPut("ActualizarInvernadero")]
+        public async Task<IActionResult> ActualizarInvernadero(InvernaderoModel invernaderomodel)
+        {
+            try
+            {
+                var resultado = await _iinvernaderoService.ActualizarInvernadero(invernaderomodel, invernaderomodel.Usuarios, invernaderomodel.Sensores);
 
-        // Devuelves el invernadero actualizado o una confirmación de éxito
-        return Ok (resultado); // O un mensaje simple si prefieres: return Ok("Invernadero actualizado correctamente.");
-      } catch (Exception ex) {
-        // Manejo de excepciones, puedes devolver un 500 si algo sale mal
-        return StatusCode (500, $"Error interno: {ex.Message}");
-      }
-    }
-
-
-
-
-    // POST: api/Invernadero/AgregarInvernadero
-    [HttpPost ("AgregarInvernadero")]
-    public async Task<IActionResult> AgregarInvernadero ([FromBody] InvernaderoModel invernadero) {
-      try {
-        // Aquí pasamos la lista de usuarios
-        var usuarios = invernadero.Usuarios;
-
-        // Aquí pasamos la lista de tipos de sensores
-        var tiposSensores = invernadero.Sensores; // asumiendo que InvernaderoModel tiene una propiedad 'Sensores' que es una lista de tipos de sensores
-
-        // Llamamos al servicio para agregar el invernadero, enlazar usuarios y sensores
-        var result = await _iinvernaderoService.AgregarInvernaderoAsync (invernadero, usuarios, tiposSensores);
-
-        return Ok (result);
-      } catch (Exception ex) {
-        // Si ocurre algún error, retornar un mensaje de error
-        return BadRequest ($"Error: {ex.Message}");
-      }
-    }
+                // Devuelves el invernadero actualizado o una confirmación de éxito
+                return Ok(resultado); // O un mensaje simple si prefieres: return Ok("Invernadero actualizado correctamente.");
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones, puedes devolver un 500 si algo sale mal
+                return StatusCode(500, $"Error interno: {ex.Message}");
+            }
+        }
 
 
 
 
+        // POST: api/Invernadero/AgregarInvernadero
+        [HttpPost("AgregarInvernadero")]
+        public async Task<IActionResult> AgregarInvernadero([FromBody] InvernaderoModel invernadero)
+        {
+            try
+            {
+                // Aquí pasamos la lista de usuarios
+                var usuarios = invernadero.Usuarios;
+
+                // Aquí pasamos la lista de tipos de sensores
+                var tiposSensores = invernadero.Sensores; // asumiendo que InvernaderoModel tiene una propiedad 'Sensores' que es una lista de tipos de sensores
+
+                // Llamamos al servicio para agregar el invernadero, enlazar usuarios y sensores
+                var result = await _iinvernaderoService.AgregarInvernaderoAsync(invernadero, usuarios, tiposSensores);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                // Si ocurre algún error, retornar un mensaje de error
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
 
 
 
-    // GET: api/Invernadero/Usuarios
-    [HttpGet ("UsuariosConInvernaderos")]
-    public async Task<IActionResult> ObtenerUsuariosConInvernaderos () {
-      try {
-        var usuarios = await _iinvernaderoService.ObtenerUsuariosConInvernaderos ();
-        return Ok (usuarios);
-      } catch (Exception ex) {
-        return StatusCode (500, $"Error al obtener los usuarios: {ex.Message}");
-      }
-    }
+
+
+
+
+        // GET: api/Invernadero/Usuarios
+        [HttpGet("UsuariosConInvernaderos")]
+        public async Task<IActionResult> ObtenerUsuariosConInvernaderos()
+        {
+            try
+            {
+                var usuarios = await _iinvernaderoService.ObtenerUsuariosConInvernaderos();
+                return Ok(usuarios);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error al obtener los usuarios: {ex.Message}");
+            }
+        }
 
 
 
@@ -155,25 +169,27 @@ namespace InvernaderoInteligente.Controllers {
             {
                 // Si ocurre un error, retornamos un mensaje de error
                 return BadRequest(new { message = $"Error: {ex.Message}" });
-      }
+            }
+        }
+
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<InvernaderoDTO>>> GetInvernaderos()
+        {
+            var invernaderos = await _iinvernaderoService.ObtenerTodosInvernaderosAsync();
+            if (invernaderos == null || invernaderos.Count == 0)
+            {
+                return NotFound("No se encontraron invernaderos.");
+            }
+
+            return Ok(invernaderos);
+        }
+
+
+
+
+
     }
-
-
-
-
-    [HttpGet]
-    public async Task<ActionResult<List<InvernaderoDTO>>> GetInvernaderos () {
-      var invernaderos = await _iinvernaderoService.ObtenerTodosInvernaderosAsync();
-      if (invernaderos == null || invernaderos.Count == 0) {
-        return NotFound ("No se encontraron invernaderos.");
-      }
-
-      return Ok (invernaderos);
-    }
-
-
-
-
-
-  }
 }
